e99b6ac20fd826cd340189874e2d31cc41449854
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-05-20 22:29:42 -0300

    20/05
    começando api de fato, inserindo dados que são os resultados das buscas, aprendendo a manipular as informações

--- api.js
+++ api.js
@@ -1,15 +1,25 @@
 const key = '8619ca13884a062b0692e3e1bc1bcebc';
 
 
-async function buscarCidade (cidadeInserida){
-    const apiBuscarDados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidadeInserida}&appid=${key}`).then(resposta => resposta.json());
-
-    console.log(apiBuscarDados);
-}
-
-function cliqueNoBotaoBuscar () {
+function cliqueNoBotaoBuscar() {
     const cidadeInserida = document.getElementById('informacao-cidade').value;
 
     buscarCidade(cidadeInserida);
 }
+
+async function buscarCidade(cidadeInserida){
+    const apiBuscarDados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidadeInserida}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json());
+
+    inserirDadosNaTela(apiBuscarDados);
+    console.log(apiBuscarDados);
+}
+
+ function inserirDadosNaTela(apiBuscarDados) {
+
+     document.querySelector('.previsao-cidade').innerHTML = "Tempo em: " + apiBuscarDados.name
+     document.querySelector('.tempo').innerHTML = apiBuscarDados.weather[0].description
+
+}
+
+
 
