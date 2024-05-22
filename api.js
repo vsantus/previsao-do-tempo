c27cf0b274928091c1d4c05e18078c9a946e137e
Vinicius Santos <viniciusantosd.abreu@gmail.com>
2024-05-21 21:49:25 -0300

    21/05 (finalizado)
    aprendi a importar os itens e dados da api e concatenar com os dados do arquivo js.

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
-const key = '8619ca13884a062b0692e3e1bc1bcebc';
-
+const key = '8619ca13884a062b0692e3e1bc1bcebc'
 
 function cliqueNoBotaoBuscar() {
     const cidadeInserida = document.getElementById('informacao-cidade').value;
@@ -16,9 +15,12 @@ async function buscarCidade(cidadeInserida){
 
  function inserirDadosNaTela(apiBuscarDados) {
 
-     document.querySelector('.previsao-cidade').innerHTML = "Tempo em: " + apiBuscarDados.name
+     document.querySelector('.previsao-cidade').innerHTML = "Tempo em: " + apiBuscarDados.name;
+     document.querySelector('.graus').innerHTML = Math.floor(apiBuscarDados.main.temp) + '°C';
+     var imagemSite = apiBuscarDados.weather[0].icon
+     document.querySelector('.imagem').src = `https://openweathermap.org/img/wn/${imagemSite}.png`
      document.querySelector('.tempo').innerHTML = apiBuscarDados.weather[0].description
-
+     document.querySelector('.umidade').innerHTML = 'Umidade: ' + apiBuscarDados.main.humidity + '%';
 }
 
 
